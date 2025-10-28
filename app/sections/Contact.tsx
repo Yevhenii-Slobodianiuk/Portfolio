7bdd23c442a565095dcf94baae47641d21011c63
Yevhenii <evgeniy.slobod9@gmail.com>
2025-10-28 15:21:02 +0200

    intagrated emailjs

--- app/sections/Contact.tsx
+++ app/sections/Contact.tsx
@@ -1,22 +1,51 @@
+"use client";
+
+import { useEffect } from "react";
 import Container from "../components/Container";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  useEffect(() => {
+    emailjs.init(process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!);
+  }, []);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+        e.currentTarget,
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
+      )
+      .then(
+        () => console.log("Message sent successfully"),
+        (error) => console.error("Error sending message:", error)
+      );
+
+    e.currentTarget.reset();
+  };
+
   return (
     <section className="bg-[#2E2F39] pt-10 pb-5 w-full" id="contact">
       <Container>
         <form
-          action=""
+          onSubmit={sendEmail}
           className="flex flex-col gap-5 m-auto mb-[100px] max-w-[500px]"
         >
           <input
-            type="text"
+            type="email"
+            name="email"
             placeholder="Your email"
-            className="px-4 py-2 border rounded-xl max-w-[500px]"
+            className="px-4 py-2 border rounded-xl"
+            required
           />
-          <input
-            type="text"
+          <textarea
+            name="message"
             placeholder="Enter message"
-            className="px-4 py-2 border rounded-xl max-w-[500px]"
+            className="px-4 py-2 border rounded-xl min-h-[100px]"
+            required
           />
           <button className="block bg-white hover:bg-transparent px-4 py-2 border border-amber-50 hover:border-[#F9E84E] rounded-xl text-[#171717] hover:text-white transition duration-300">
             contact me
